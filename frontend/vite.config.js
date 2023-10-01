393d52f4f1e90344fa2814115f67331c3b09b8f1
Yuji_Itadorii <rawat141abhay@gmail.com>
2023-10-01 11:39:50 +0530

    adding

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// add the beginning of your app entry
+import "vite/modulepreload-polyfill";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    // generate manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: "/path/to/main.js",
+    },
+  },
+});
